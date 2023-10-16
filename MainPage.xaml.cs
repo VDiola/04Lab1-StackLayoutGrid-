42541ad9dd7b00aaae895c34149cb9f421bfda5d
VDiola <126273351+VDiola@users.noreply.github.com>
2023-10-16 09:00:56 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MauiApp4StackLayoutGrid;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnVerticalStart_Clicked(object sender, EventArgs e)
+	{
+		Target.VerticalOptions = LayoutOptions.Start;
+	}
+
+	private void OnVerticalCenter_Clicked(object sender, EventArgs e)
+	{
+		Target.VerticalOptions = LayoutOptions.Center;
+	}
+
+	private void OnVerticalEnd_Clicked(object sender, EventArgs e)
+	{
+		Target.VerticalOptions = LayoutOptions.End;
+	}
+
+	private void OnHorizontalStart_Clicked(object sender, EventArgs e)
+	{
+		Target.HorizontalOptions = LayoutOptions.Start;
+	}
+
+	private void OnHorizontalCenter_Clicked(object sender, EventArgs e)
+	{
+		Target.HorizontalOptions = LayoutOptions.Center;
+	}
+
+	private void OnHorizontalEnd_Clicked(object sender, EventArgs e)
+	{
+		Target.HorizontalOptions = LayoutOptions.End;
+	}
+}
+
